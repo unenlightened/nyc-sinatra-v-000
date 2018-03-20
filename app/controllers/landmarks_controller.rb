5f6d077339382b8fdeb340c63aeab7ded424c3a3
Chely Ho <adept_inept@yahoo.com>
2018-03-20 19:14:18 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -26,16 +26,7 @@ class LandmarksController < ApplicationController
 
   patch '/landmarks/:id' do
     @landmark = Landmark.find_by_id(params[:id])
-    @landmark.name = params[:landmark][:name]
-
-    @landmark.titles = params[:landmark][:title_ids].map{|title| Title.find_by(name: title)} if params[:landmark][:title_ids]
-    @landmark.landmarks = params[:landmark][:landmark_ids].map{|landmark| Landmark.find_by(name: landmark)} if
-    params[:landmark][:landmark_ids]
-
-    @landmark.titles << Title.create(name: params[:title][:name]) if !params[:title][:name].empty?
-    @landmark.landmarks << Landmark.create(name: params[:landmark][:name]) if !params[:landmark][:name].empty?
-
-    @landmark.save
+    @landmark.update(params[:landmark])
 
     redirect "/landmarks/#{@landmark.id}"
   end
